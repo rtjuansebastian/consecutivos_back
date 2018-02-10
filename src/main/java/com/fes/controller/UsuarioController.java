983bc7f2171cbf82ba0b4d73faace326ac1b3a84
Juan Rodriguez <rtjuansebastian@gmail.com>
2018-02-10 10:41:59 -0500

    Cambios para desarrollo.

--- src/main/java/com/fes/controller/UsuarioController.java
+++ src/main/java/com/fes/controller/UsuarioController.java
@@ -47,7 +47,7 @@ public class UsuarioController {
 		return new ResponseEntity<Void>(headers,HttpStatus.CREATED);
 	}
 	
-	@CrossOrigin(origins = "http://159.89.94.78:3000")
+	@CrossOrigin(origins = "http://localhost:3000")
 	@GetMapping("usuarios")
 	public ResponseEntity<List<Usuario>> read(){
 		
